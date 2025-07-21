b0e4cfef7c22bded2efa5bf67679871f43e0ca74
pemako <pemakoa@gmail.com>
2025-07-21 09:35:10 +0800

    init fork from rwv/chinese-dos-games-web

---
+++ static/games/download_data.py
@@ -0,0 +1,63 @@
+import hashlib
+import inspect
+import json
+import os
+import urllib.request
+from concurrent.futures import ThreadPoolExecutor, wait
+
+root = os.path.dirname(os.path.abspath(
+    inspect.getfile(inspect.currentframe())))
+
+PREFIX = "https://dos-bin.zczc.cz/"
+DESTINATION = os.path.join(root, "bin")
+BUF_SIZE = 65536
+THREAD_SIZE = 10
+
+# read game infos from games.json
+with open(os.path.join(root, "games.json"), encoding="utf8") as f:
+    game_infos = json.load(f)
+
+
+def generate_sha256(file):
+    sha256 = hashlib.sha256()
+    with open(file, "rb") as f:
+        while True:
+            data = f.read(BUF_SIZE)
+            if not data:
+                break
+            sha256.update(data)
+    return sha256.hexdigest()
+
+
+def download(identifier, url, file):
+    print(f"Downloading {identifier} game file")
+    urllib.request.urlretrieve(url, file)
+
+
+def main(prefix=PREFIX, destination=DESTINATION):
+    # create folder
+    os.makedirs(destination, exist_ok=True)
+
+    executor = ThreadPoolExecutor(max_workers=THREAD_SIZE)
+    all_task = list()
+
+    downloaded = list()
+    for identifier in game_infos["games"].keys():
+        file = os.path.normcase(os.path.join(destination, identifier + ".zip"))
+        url = prefix + urllib.parse.quote(identifier) + ".zip"
+        if (
+            os.path.isfile(file)
+            and generate_sha256(file) == game_infos["games"][identifier]["sha256"]
+        ):
+            print(f"skip {identifier}")
+        else:
+            downloaded.append(identifier)
+            task = executor.submit(download, identifier, url, file)
+            all_task.append(task)
+
+    wait(all_task)
+    return downloaded
+
+
+if __name__ == "__main__":
+    main()
